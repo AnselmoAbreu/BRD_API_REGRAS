45f61502e53c00a8c97ad691ca3893793f957faf
AnselmoAbreu <anselmo.mda@outlook.com>
2025-05-05 14:26:53 -0300

    Remoção de Usings desnecessários

--- Services/Interfaces/ILayoutMtp240Service.cs
+++ Services/Interfaces/ILayoutMtp240Service.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BRD_API_REGRAS.Services.Interfaces
+﻿namespace BRD_API_REGRAS.Services.Interfaces
 {
     public interface ILayoutMtp240Service
     {
