07c04cae38a5d68e2ab64103ac999011ceb9857b
AnselmoAbreu <anselmo.mda@outlook.com>
2025-05-12 14:39:55 -0300

    Criação do Layout do Cobrança 240 Posições

---
+++ Services/Interfaces/ILayoutCob240Service.cs
@@ -0,0 +1,7 @@
+﻿namespace BRD_API_REGRAS.Services.Interfaces
+{
+    public interface ILayoutCob240Service
+    {
+        dynamic MontarJsonCob240(dynamic dynamicList, string codigoArquivo);
+    }
+}
