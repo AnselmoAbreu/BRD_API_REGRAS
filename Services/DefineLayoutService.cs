542362570794adf0660a37479e53bff59d173e07
AnselmoAbreu <anselmo.mda@outlook.com>
2025-05-12 14:49:50 -0300

    Criada a opção do COB240

--- Services/DefineLayoutService.cs
+++ Services/DefineLayoutService.cs
@@ -7,6 +7,7 @@ namespace BRD_API_REGRAS.Services
     public class DefineLayoutService
     {
         readonly LayoutCob400Service cobranca400 = new LayoutCob400Service();
+        readonly LayoutCob240Service cobranca240 = new LayoutCob240Service();
         readonly LayoutMtp240Service mtp240 = new LayoutMtp240Service();
 
         public string MontarJson(string codigoArquivo)
@@ -14,6 +15,9 @@ namespace BRD_API_REGRAS.Services
             dynamic dynamicList = new ExpandoObject();
             switch (codigoArquivo)
             {
+                case "COB240":
+                    cobranca240.MontarJsonCob240(dynamicList, codigoArquivo);
+                    break;
                 case "COB400":
                     cobranca400.MontarJsonCob400(dynamicList, codigoArquivo);
                     break;
