73a800df1ace090ab86128d54a747c504c938ad1
AnselmoAbreu <anselmo.mda@outlook.com>
2025-05-14 12:36:28 -0300

    Ajustes nos layouts depois de revisão com o manual

--- Services/LayoutCob240Service.cs
+++ Services/LayoutCob240Service.cs
@@ -1,10 +1,5 @@
 ﻿using BRD_API_REGRAS.Services.Interfaces;
-using Microsoft.Win32;
-using Swashbuckle.Swagger;
-using System.Drawing;
 using System.Dynamic;
-using System.Runtime.ConstrainedExecution;
-using System.Web.Helpers;
 
 namespace BRD_API_REGRAS.Services
 {
@@ -149,43 +144,43 @@ namespace BRD_API_REGRAS.Services
             // ========================================================================
 
             // Segmento 3P
-            cdObject3P.Banco = "1:003:N:R:0:1-3::Erro - Código do Banco na Compensação::E001";
-            cdObject3P.Lote = "4:004:N:R:0:4-7::Erro - Lote de Serviço::E002";
-            cdObject3P.Registro = "8:001:N:R:0:8-8:3:Erro - Tipo de Registro::E003";
-            cdObject3P.NºRegistro = "9:005:N:R:0:9-13::Erro - Nº Sequencial do Registro no Lote::E038";
-            cdObject3P.Segmento = "14:001:A:R:0:14-14:P:Erro - Cód. Segmento do Registro Detalhe::E039";
-            cdObject3P.Serviço = "15:001:A:R:0:15-15::Erro - CNAB::E004";
-            cdObject3P.CódMov = "16:002:N:R:0:16-17::Erro - Código de Movimento Remessa::E004";
-            cdObject3P.Agência = "18:005:N:R:0:18-22::Erro - Agência Mantenedora da Conta::E008";
-            cdObject3P.AgênciaDV = "23:001:A:R:0:23-23::Erro - Digito Verificador da Agência::E009";
-            cdObject3P.Conta = "24:012:N:R:0:24-35::Erro - Número da Conta-Corrente::E010";
-            cdObject3P.ContaDV = "36:001:A:R:0:36-36::Erro - Digito Verificador da Conta::E011";
-            cdObject3P.AgContaDV = "37:001:A:R:0:37-37::Erro - Digito Verificador da Ag/Conta::E012";
-            cdObject3P.IdentificaçãoProduto = "38:003:N:R:0:38-40::Erro - Identificação do Produto::E069";
-            cdObject3P.Zeros = "41:005:N:R:0:41-45::Erro - Zeros::E069";
-            cdObject3P.NossoNúmero = "46:011:N:R:0:46-56::Erro - Nosso Número::E069";
-            cdObject3P.DigitoNossoNúmero = "57:001:A:R:0:57-57::Erro - Digito do Nosso Número::E069";
-            cdObject3P.Carteira = "58:001:N:R:0:58-58::Erro - Código da Carteira::E006";
-            cdObject3P.Cadastro = "59:001:N:R:0:59-59::Erro - Forma de Cadastr. do Título no Banco::E007";
-            cdObject3P.TipoDocumento = "60:001:N:R:0:60-60::Erro - Tipo de Documento::E008";
-            cdObject3P.EmissãoBoleto = "61:001:N:R:0:61-61::Erro - Identificação da Emissão do Boleto::E009";
-            cdObject3P.DistribuiçãoBoleto = "62:001:N:R:0:62-62::Erro - Identificação da Distribuição::E010";
-            cdObject3P.NºDocumento = "63:015:A:R:0:63-77::Erro - Número do Documento de Cobrança::E011";
-            cdObject3P.Vencimento = "78:008:N:R:0:78-85::Erro - Data de Vencimento do Título:D:E012";
-            cdObject3P.ValorTítulo = "86:015:N:R:0:86-100::Erro - Valor Nominal do Título::E070"; // Ajustado de 13 para 15 (13+2 decimais)
-            cdObject3P.AgCobradora = "101:005:N:R:0:101-105::Erro - Agência Encarregada da Cobrança::E014";
-            cdObject3P.AgCobradoraDV = "106:001:A:R:0:106-106::Erro - Digito Verificador da Agência::E009";
-            cdObject3P.EspécieTítulo = "107:002:N:R:0:107-108::Erro - Espécie do Título::E015";
-            cdObject3P.Aceite = "109:001:A:R:0:109-109::Erro - Identific. de Título Aceito/Não Aceito::E016";
-            cdObject3P.DataEmissãoTítulo = "110:008:N:R:0:110-117::Erro - Data da Emissão do Título:D:E071";
-            cdObject3P.CódJurosMora = "118:001:N:R:0:118-118::Erro - Código do Juros de Mora::E018";
-            cdObject3P.DataJurosMora = "119:008:N:R:0:119-126::Erro - Data do Juros de Mora:D:E019";
-            cdObject3P.JurosMora = "127:015:N:R:0:127-141::Erro - Juros de Mora por Dia/Taxa::E020"; // Ajustado de 13 para 15 (13+2 decimais)
-            cdObject3P.CódDesc1 = "142:001:N:R:0:142-142::Erro - Código do Desconto 1::E021";
-            cdObject3P.DataDesc1 = "143:008:N:R:0:143-150::Erro - Data do Desconto 1:D:E022";
-            cdObject3P.Desconto1 = "151:015:N:R:0:151-165::Erro - Valor/Percentual a ser Concedido::E023"; // Ajustado de 13 para 15 (13+2 decimais)
-            cdObject3P.ValorICF = "166:015:N:R:0:166-180::Erro - Valor do ICF a ser Recolhido::E024"; // Ajustado de 13 para 15 (13+2 decimais)
-            cdObject3P.ValorAbatimento = "181:015:N:R:0:181-195::Erro - Valor do Abatimento::E045"; // Ajustado de 13 para 15 (13+2 decimais)
+            cdObject3P.Banco = "1:003:N:R:0:1-3::Erro - Código do Banco na Compensação::G001";
+            cdObject3P.Lote = "4:004:N:R:0:4-7::Erro - Lote de Serviço::G002";
+            cdObject3P.Registro = "8:001:N:R:0:8-8:3:Erro - Tipo de Registro::G003";
+            cdObject3P.NºRegistro = "9:005:N:R:0:9-13::Erro - Nº Sequencial do Registro no Lote::G038";
+            cdObject3P.Segmento = "14:001:A:R:0:14-14:P:Erro - Cód. Segmento do Registro Detalhe::G039";
+            cdObject3P.Serviço = "15:001:A:R:0:15-15::Erro - CNAB::G004";
+            cdObject3P.CódMov = "16:002:N:R:0:16-17::Erro - Código de Movimento Remessa::C004";
+            cdObject3P.Agência = "18:005:N:R:0:18-22::Erro - Agência Mantenedora da Conta::G008";
+            cdObject3P.AgênciaDV = "23:001:A:R:0:23-23::Erro - Digito Verificador da Agência::G009";
+            cdObject3P.Conta = "24:012:N:R:0:24-35::Erro - Número da Conta-Corrente::G010";
+            cdObject3P.ContaDV = "36:001:A:R:0:36-36::Erro - Digito Verificador da Conta::G011";
+            cdObject3P.AgContaDV = "37:001:A:R:0:37-37::Erro - Digito Verificador da Ag/Conta::G012";
+            cdObject3P.IdentificaçãoProduto = "38:003:N:R:0:38-40::Erro - Identificação do Produto::G069";
+            cdObject3P.Zeros = "41:005:N:R:0:41-45::Erro - Zeros::G069";
+            cdObject3P.NossoNúmero = "46:011:N:R:0:46-56::Erro - Nosso Número::G069";
+            cdObject3P.DigitoNossoNúmero = "57:001:A:R:0:57-57::Erro - Digito do Nosso Número::G069";
+            cdObject3P.Carteira = "58:001:N:R:0:58-58::Erro - Código da Carteira::C006";
+            cdObject3P.Cadastro = "59:001:N:R:0:59-59::Erro - Forma de Cadastr. do Título no Banco::C007";
+            cdObject3P.TipoDocumento = "60:001:N:R:0:60-60::Erro - Tipo de Documento::C008";
+            cdObject3P.EmissãoBoleto = "61:001:N:R:0:61-61::Erro - Identificação da Emissão do Boleto::C009";
+            cdObject3P.DistribuiçãoBoleto = "62:001:N:R:0:62-62::Erro - Identificação da Distribuição::C010";
+            cdObject3P.NºDocumento = "63:015:A:R:0:63-77::Erro - Número do Documento de Cobrança::C011";
+            cdObject3P.Vencimento = "78:008:N:R:0:78-85::Erro - Data de Vencimento do Título:D:C012";
+            cdObject3P.ValorTítulo = "86:015:N:R:0:86-100::Erro - Valor Nominal do Título::G070"; // Ajustado de 13 para 15 (13+2 decimais)
+            cdObject3P.AgCobradora = "101:005:N:R:0:101-105::Erro - Agência Encarregada da Cobrança::C014";
+            cdObject3P.AgCobradoraDV = "106:001:A:R:0:106-106::Erro - Digito Verificador da Agência::G009";
+            cdObject3P.EspécieTítulo = "107:002:N:R:0:107-108::Erro - Espécie do Título::C015";
+            cdObject3P.Aceite = "109:001:A:R:0:109-109::Erro - Identific. de Título Aceito/Não Aceito::C016";
+            cdObject3P.DataEmissãoTítulo = "110:008:N:R:0:110-117::Erro - Data da Emissão do Título:D:G071";
+            cdObject3P.CódJurosMora = "118:001:N:R:0:118-118::Erro - Código do Juros de Mora::C018";
+            cdObject3P.DataJurosMora = "119:008:N:R:0:119-126::Erro - Data do Juros de Mora:D:C019";
+            cdObject3P.JurosMora = "127:015:N:R:0:127-141::Erro - Juros de Mora por Dia/Taxa::C020"; // Ajustado de 13 para 15 (13+2 decimais)
+            cdObject3P.CódDesc1 = "142:001:N:R:0:142-142::Erro - Código do Desconto 1::C021";
+            cdObject3P.DataDesc1 = "143:008:N:R:0:143-150::Erro - Data do Desconto 1:D:C022";
+            cdObject3P.Desconto1 = "151:015:N:R:0:151-165::Erro - Valor/Percentual a ser Concedido::C023"; // Ajustado de 13 para 15 (13+2 decimais)
+            cdObject3P.ValorICF = "166:015:N:R:0:166-180::Erro - Valor do ICF a ser Recolhido::C024"; // Ajustado de 13 para 15 (13+2 decimais)
+            cdObject3P.ValorAbatimento = "181:015:N:R:0:181-195::Erro - Valor do Abatimento::G045"; // Ajustado de 13 para 15 (13+2 decimais)
             cdObject3P.UsoEmpresaBeneficiario = "196:025:A:R:0:196-220::Erro - Identificação do Título na Empresa::G072";
             cdObject3P.CodigoProtesto = "221:001:N:R:0:221-221::Erro - Código para Protesto::C026";
             cdObject3P.PrazoProtesto = "222:002:N:R:0:222-223::Erro - Número de Dias para Protesto::C027";
@@ -210,7 +205,7 @@ namespace BRD_API_REGRAS.Services
             cdObject3R.Desconto2 = "27:015:N:R:0:27-41::Erro - Valor/Percentual a ser Concedido::C023"; // 13+2 decimais
             cdObject3R.CódDesc3 = "42:001:N:R:0:42-42::Erro - Código do Desconto 3::C021";
             cdObject3R.DataDesc3 = "43:008:N:R:0:43-50::Erro - Data do Desconto 3:D:C022";
-            cdObject3R.Desconto3 = "51:015:N:R:0:51-65::Erro - Valor/Percentual a ser Concedido::C033"; // 13+2 decimais
+            cdObject3R.Desconto3 = "51:015:N:R:0:51-65::Erro - Valor/Percentual a ser Concedido::C023"; // 13+2 decimais
             cdObject3R.CódMulta = "66:001:N:R:0:66-66::Erro - Código da Multa::G073";
             cdObject3R.DataMulta = "67:008:N:R:0:67-74::Erro - Data da Multa:D:G074";
             cdObject3R.ValorMulta = "75:015:N:R:0:75-89::Erro - Valor/Percentual a ser Aplicado::G075"; // 13+2 decimais
